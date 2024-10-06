4e6f0587ab48dc350a4bff3ac5d3b4daacc15a9a
Shivam Rana <60025558+Shivamrana706@users.noreply.github.com>
2024-10-06 18:05:55 -0400

    all done except css2 ENV3

--- src/app/component/Header.tsx
+++ src/app/component/Header.tsx
@@ -7,7 +7,9 @@ import axios from 'axios';
 import { WEATHER_API_KEY } from "../../constant.js";
 import { useDispatch } from 'react-redux';
 
+
 import { addForcastWeatherData, addWeatherData } from "../redux/slice.js";
+import MobileDialog from "./mobileDialog";
 
 interface YourComponentProps {
     location: string
@@ -37,15 +39,17 @@ const Header: React.FC<YourComponentProps> = ({ location }) => {
                 console.error('Error status:', error.response.status);
             }
         }
+    }
+    const onMobileButtonClick = () => {
 
     }
     return (
         <div className="flex p-3 justify-between items-center text-white sm:w-full">
             <div className="flex text-center items-center">
-                <p className="text-3xl mx-2 font-semibold ">Weather</p>
+                <p className="sm:text-3xl sm:mx-2 sm:font-semibold text-2xl ">Weather</p>
                 <IoSunnyOutline size={25} color="yellow" />
             </div>
-            <div className="flex items-center text-white justify-evenly ">
+            <div className="sm:flex sm:items-center sm:text-white sm:justify-evenly flex   ">
                 <div className="sm:flex items-center hidden "><FaLocationCrosshairs />
                     <p className="mr-2 ml-1 font-serif">
                         {
@@ -55,12 +59,13 @@ const Header: React.FC<YourComponentProps> = ({ location }) => {
                     </p>
                 </div>
                 <input
-                    className="mr-1 rounded-full px-3 py-1 text-black font-mono "
+                    className="mr-1 rounded-full px-2 sm:px-3 py-1 text-black font-mono "
                     type="text"
                     value={search}
                     onChange={(e) => setSearch(e.target.value)}
                 />
-                <IoSearchCircleSharp onClick={onButtonClick} className="mr-2" color="yellow" size={40} />
+
+                <IoSearchCircleSharp onClick={onButtonClick} className="mr-2" color="white" size={40} />
             </div>
         </div>
     )
